6c59be2f1f79d755ee53ee172d0743719cea8dcd
betiiy-haile <betelhemhaile74@gmail.com>
2023-05-06 19:43:06 +0300

    Adding the cart-context and using the useContext

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     return (
       <Fragment>
         <header className={classes.header}>
-          <h1>ReactMeals</h1>
+          <h1>Order-Meals</h1>
           <HeaderCartButton onclick={props.onclick} />
         </header>
         <div className={classes["main-image"]}>
